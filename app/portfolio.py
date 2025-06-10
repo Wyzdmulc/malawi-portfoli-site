df6a8707ae958f2d890b23779f5cf2d5aacf3ec5
Wyzdmulc <wisdomlchinkondenji@gmail.com>
2025-06-10 07:30:02 +0200

    Create portfolio.py

---
+++ app/portfolio.py
@@ -0,0 +1,6 @@
+def optimize_portfolio(df):
+    return {'expected_return': 0.12, 'volatility': 0.08}, 'portfolio_plot.png'
+
+def high_dividend_portfolio(df):
+    return {'top_dividends': ['STOCK1', 'STOCK2']}, 'dividend_plot.png'
+  
